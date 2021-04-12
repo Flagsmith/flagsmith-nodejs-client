5bc173b3baf07a3f5f8ac2f0b2b4ee6b811db109
Thomas Jouannic <thomas.jouannic@mistertemp.com>
2021-04-12 15:16:29 +0200

    Add "prettier" to use the same format along the entire project

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    printWidth: 100,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none',
+    useTabs: false,
+    arrowParens: 'avoid'
+};
