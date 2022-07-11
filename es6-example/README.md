c04c4e3ce864e729cf73b3cfeee5d3c0d7c09638
kyle-ssg <kyle@solidstategroup.com>
2022-07-11 14:52:44 +0100

    ES6 example

---
+++ es6-example/README.md
@@ -0,0 +1,62 @@
+# Nodejs-ES6-Boilerplate
+[![Made in Indonesia](https://made-in-indonesia.github.io/made-in-indonesia.svg)](https://github.com/made-in-indonesia/made-in-indonesia)
+
+A Node.js boilerplate with ES6, ESLint, and Prettier
+
+## Background
+
+I created this boilerplate because I have several coding interviews that needs to be done using JavaScript.
+
+## Scripts
+
+```bash
+# Run the project without nodemon
+npm run start
+
+# Build the project
+npm run build
+
+# Clean build
+npm run clean
+
+# Check the lint errors
+npm run lint
+
+# Fix the lint errors
+npm run lint:fix
+
+# Run prettier
+npm run format
+
+# Check prettier errors
+npm run format:check
+```
+
+## Usage
+
+1. Clone the repository and init new git project
+
+```bash
+$ git clone git@github.com:vferdiansyah/nodejs-es6-boilerplate.git ./your/project/folder
+$ cd ./your/project/folder
+$ rm -rf .git
+$ git init
+```
+
+2. Change project specific information in the following places
+
+- [package.json](./package.json)
+- [README.md](./README.md)
+
+3. Install dependencies
+
+```bash
+$ npm i
+```
+
+4. Start coding
+
+## License
+
+Copyright (c) 2020 Veri Ferdiansyah  
+Licensed under the MIT license.
---
+++ es6-example/src/api/index.js
@@ -0,0 +1,49 @@
+import {Router}  from 'express'
+import { Flagsmith }  from '../../../build'
+const environmentKey = '';
+import nodecache from "node-cache";
+
+if (!environmentKey) {
+    throw new Error(
+        'Please generate a Server Side SDK Key in environment settings to run the example'
+    );
+}
+const flagsmith = new Flagsmith({
+    environmentKey,
+    enableLocalEvaluation: true,
+    cache: new nodecache({
+        stdTTL: 10,
+        checkperiod: 10,
+    }),
+});
+
+
+// solicitor
+
+// read only
+ // sra authorisation date
+
+const api =  () => {
+    const api = Router();
+
+    api.get('/', async (req, res) => {
+        const flags = await flagsmith.getEnvironmentFlags();
+        res.json(flags);
+    });
+
+    api.get('/:user', async (req, res) => {
+        const flags = await flagsmith.getIdentityFlags(req.params.user, { checkout_v2: 1 });
+        const fontSize = flags.getFeatureValue('font_size');
+        const checkoutV2 = flags.isFeatureEnabled('checkout_v2');
+        res.json({ fontSize, checkoutV2 });
+    });
+
+    api.get('/:user/segments', async (req, res) => {
+        const segments = await flagsmith.getIdentitySegments(req.params.user, { checkout_v2: 1 });
+        res.json(segments.map(v => v.name));
+    });
+
+    return api;
+};
+
+export default api
