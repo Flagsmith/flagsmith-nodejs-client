b2ce413b391d729fd81f5539600d3e14a0a85fbd
Matthew Elwell <matthewe@solidstategroup.com>
2022-07-13 12:41:42 +0100

    Use featureName for analytics (#48)

--- tests/sdk/analytics.test.ts
+++ tests/sdk/analytics.test.ts
@@ -8,28 +8,28 @@ afterEach(() => {
 
 test('test_analytics_processor_track_feature_updates_analytics_data', () => {
     const aP = analyticsProcessor();
-    aP.trackFeature(1);
-    expect(aP.analyticsData[1]).toBe(1);
+    aP.trackFeature("myFeature");
+    expect(aP.analyticsData["myFeature"]).toBe(1);
 
-    aP.trackFeature(1);
-    expect(aP.analyticsData[1]).toBe(2);
+    aP.trackFeature("myFeature");
+    expect(aP.analyticsData["myFeature"]).toBe(2);
 });
 
 test('test_analytics_processor_flush_clears_analytics_data', async () => {
     const aP = analyticsProcessor();
-    aP.trackFeature(1);
+    aP.trackFeature("myFeature");
     await aP.flush();
     expect(aP.analyticsData).toStrictEqual({});
 });
 
 test('test_analytics_processor_flush_post_request_data_match_ananlytics_data', async () => {
     const aP = analyticsProcessor();
-    aP.trackFeature(1);
-    aP.trackFeature(2);
+    aP.trackFeature("myFeature1");
+    aP.trackFeature("myFeature2");
     await aP.flush();
     expect(fetch).toHaveBeenCalledTimes(1);
     expect(fetch).toHaveBeenCalledWith('http://testUrlanalytics/flags/', {
-        body: '{"1":1,"2":1}',
+        body: '{"myFeature1":1,"myFeature2":1}',
         headers: { 'Content-Type': 'application/json', 'X-Environment-Key': 'test-key' },
         method: 'POST',
         timeout: 3
@@ -39,9 +39,9 @@ test('test_analytics_processor_flush_post_request_data_match_ananlytics_data', a
 jest.useFakeTimers()
 test('test_analytics_processor_flush_post_request_data_match_ananlytics_data_test', async () => {
     const aP = analyticsProcessor();
-    aP.trackFeature(1);
+    aP.trackFeature("myFeature1");
     setTimeout(() => {
-        aP.trackFeature(2);
+        aP.trackFeature("myFeature2");
         expect(fetch).toHaveBeenCalledTimes(1);
     }, 15000);
     jest.runOnlyPendingTimers();
