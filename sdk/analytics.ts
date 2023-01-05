b0a336d0efd87f5cdee5313cbcedba88f315e094
Matthew Elwell <matthew.elwell@flagsmith.com>
2023-01-05 11:50:28 +0000

    Swallow errors arising from fetch in analytics (#95)
    * Swallow errors arising from fetch in analytics
    
    * Use try / catch instead of then / catch
    
    * Add pino logger
    
    * Add comments to the test

--- sdk/analytics.ts
+++ sdk/analytics.ts
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import pino, { Logger } from 'pino';
 
 const ANALYTICS_ENDPOINT = 'analytics/flags/';
 
@@ -11,21 +12,24 @@ export class AnalyticsProcessor {
     private lastFlushed: number;
     analyticsData: { [key: string]: any };
     private requestTimeoutMs: number = 3000;
+    private logger: Logger;
+
     /**
-     * AnalyticsProcessor is used to track how often individual Flags are evaluated within 
+     * AnalyticsProcessor is used to track how often individual Flags are evaluated within
      * the Flagsmith SDK. Docs: https://docs.flagsmith.com/advanced-use/flag-analytics.
-     * 
+     *
      * @param data.environmentKey environment key obtained from the Flagsmith UI
      * @param data.baseApiUrl base api url to override when using self hosted version
      * @param data.requestTimeoutMs used to tell requests to stop waiting for a response after a
             given number of milliseconds
      */
-    constructor(data: { environmentKey: string; baseApiUrl: string; requestTimeoutMs?: number }) {
+    constructor(data: { environmentKey: string; baseApiUrl: string; requestTimeoutMs?: number, logger?: Logger }) {
         this.analyticsEndpoint = data.baseApiUrl + ANALYTICS_ENDPOINT;
         this.environmentKey = data.environmentKey;
         this.lastFlushed = Date.now();
         this.analyticsData = {};
         this.requestTimeoutMs = data.requestTimeoutMs || this.requestTimeoutMs;
+        this.logger = data.logger || pino();
     }
     /**
      * Sends all the collected data to the api asynchronously and resets the timer
@@ -35,15 +39,22 @@ export class AnalyticsProcessor {
             return;
         }
 
-        await fetch(this.analyticsEndpoint, {
-            method: 'POST',
-            body: JSON.stringify(this.analyticsData),
-            timeout: this.requestTimeoutMs,
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Environment-Key': this.environmentKey
-            }
-        });
+        try {
+            await fetch(this.analyticsEndpoint, {
+                method: 'POST',
+                body: JSON.stringify(this.analyticsData),
+                timeout: this.requestTimeoutMs,
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Environment-Key': this.environmentKey
+                }
+            })
+        } catch (error) {
+            // We don't want failing to write analytics to cause any exceptions in the main
+            // thread so we just swallow them here.
+            this.logger.warn('Failed to post analytics to Flagsmith API. Not clearing data, will retry.')
+            return;
+        }
 
         this.analyticsData = {};
         this.lastFlushed = Date.now();
