2cc95c7ca1b49037fb8af3cf77e71bc816f24230
Rodrigo López Dato <rodrigo.lopezdato@flagsmith.com>
2024-11-07 10:56:24 -0300

    feat!: Remove all uses of CJS, add named Flagsmith export (#163)
    * Remove all uses of `require`
    
    * Use only CJS exports
    
    * Add named Flagsmith export
    
    * Remove md5, big-integer deps
    
    * Remove esModuleInterop
    
    * Import test data instead of reading filesystem
    
    * Remove unused default export
    
    * convert hex to decimal with native bigint
    
    * remove uuid dependency
    
    * Upgrade GHA workflow dependencies
    
    * Version 4.0.0
    
    * Remove redundant Number cast

--- tests/engine/e2e/engine.test.ts
+++ tests/engine/e2e/engine.test.ts
@@ -1,18 +1,17 @@
-import { readFileSync } from 'fs';
 import { getIdentityFeatureStates } from '../../../flagsmith-engine/index.js';
 import { EnvironmentModel } from '../../../flagsmith-engine/environments/models.js';
 import { buildEnvironmentModel } from '../../../flagsmith-engine/environments/util.js';
 import { IdentityModel } from '../../../flagsmith-engine/identities/models.js';
 import { buildIdentityModel } from '../../../flagsmith-engine/identities/util.js';
+import * as testData from '../engine-tests/engine-test-data/data/environment_n9fbf9h3v4fFgH3U3ngWhb.json'
 
 function extractTestCases(
-    filePath: string
+    data: any
 ): {
     environment: EnvironmentModel;
     identity: IdentityModel;
     response: any;
 }[] {
-    const data = JSON.parse(readFileSync(filePath, 'utf-8'));
     const environmentModel = buildEnvironmentModel(data['environment']);
     const test_data = data['identities_and_responses'].map((test_case: any) => {
         const identity = buildIdentityModel(test_case['identity']);
@@ -27,9 +26,7 @@ function extractTestCases(
 }
 
 test('Test Engine', () => {
-    const testCases = extractTestCases(
-        __dirname + '/../engine-tests/engine-test-data/data/environment_n9fbf9h3v4fFgH3U3ngWhb.json'
-    );
+    const testCases = extractTestCases(testData);
     for (const testCase of testCases) {
         const engine_response = getIdentityFeatureStates(testCase.environment, testCase.identity);
         const sortedEngineFlags = engine_response.sort((a, b) =>
